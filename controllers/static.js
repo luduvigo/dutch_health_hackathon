f8c99409a7fdb48320a44773d4581973c0696022
luduvigo <paoloantoniorossi@gmail.com>
2015-09-06 00:10:39 +0200

    fixes

--- controllers/static.js
+++ controllers/static.js
@@ -6,9 +6,22 @@ router.get("/", function(req, res){
 	res.sendfile("layouts/index.html")
 })
 
-router.get("/user", function(req, res){
-	res.sendfile("layouts/patient_easy.html")
+router.get("/user/1", function(req, res){
+	res.sendfile("layouts/user.html")
 })
+
+router.get("/user/2", function(req, res){
+	res.sendfile("layouts/user.html")
+})
+
+router.get("/user/3", function(req, res){
+	res.sendfile("layouts/user.html")
+})
+
+router.get("/user/4", function(req, res){
+	res.sendfile("layouts/user.html")
+})
+
 
 router.get("/expert", function(req, res){
 	res.sendfile("layouts/patient_medium.html")

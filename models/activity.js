3957f3352d636867d327101a6fc0671912c9ce98
luduvigo <paoloantoniorossi@gmail.com>
2015-09-05 16:45:16 +0200

    Add done time

--- models/activity.js
+++ models/activity.js
@@ -2,7 +2,8 @@ var db = require("../db")
 var Activity = db.model("Activity", {
 	pictogram:            {type: String , required: true},
     activitydate:        {type: Date,    required: true},
-    done:           {type: Boolean, required: true, default: false}
+    done:                {type: Boolean, required: true, default: false},
+    donetime:            {type: Date}
 })
 
 module.exports = Activity

e45425bac3902e044a66ef0495979ded943e71af
luduvigo <paoloantoniorossi@gmail.com>
2015-09-05 21:55:38 +0200

    change port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.use("/api/activities", require("./controllers/api/activities"))
 app.use(require("./controllers/static"))
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 8080, function(){
       console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
